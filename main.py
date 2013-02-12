e0b08dc95b4910c3bc72d4a716ff2b4381d5db5d
Nick Collins <nick@vividx.co.uk>
2013-02-12 17:42:51 -0500

    Move the code for creating an image from text into a new function, make_text_image().
    Created two functions, glue_images_vertical() and
    splice_images_vertical() for creating new images from existing images.

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 # coding=UTF-8
 # Generate training sets for text recognition
 
+from __future__ import division
 import Image, ImageDraw, ImageFont
 
 text0 = u"Hello, world!"
 text1 = u"您好，世界！"
 
-
-im = Image.new("L",(3000,300))
-draw = ImageDraw.Draw(im)
 
 fontsize = 256
 fonts = {
@@ -19,5 +17,25 @@ fonts = {
     "verdana": ImageFont.truetype(u"/Library/Fonts/Verdana.ttf",fontsize),
 }
 
-draw.text((20, 20), text1, font=fonts["song"], fill=255)
-im_resized = im.resize((1000,100), Image.ANTIALIAS)
+def make_text_image(text, font):
+    im = Image.new("L",(3000,300))
+    draw = ImageDraw.Draw(im)
+    draw.text((20, 20), text1, font=fonts["song"], fill=255)
+    im_resized = im.resize((1000,100), Image.ANTIALIAS)
+
+
+def glue_images_vertical(im0, im1):
+    if im0.size != im1.size or im0.mode != im1.mode:
+        raise Exception
+    else:
+        im_out = Image.new(im0.mode,(im0.size[0],2*im0.size[1]))
+        im_out.paste(im0,(0,0))
+        im_out.paste(im1,(0,im0.size[1]))
+        return im_out
+
+
+def splice_images_vertical(im0, im1):
+    im_tmp = glue_images_vertical(im0, im1)
+    w, h = im_tmp.size
+    top, bottom = h // 4, h * 3 // 4
+    return im_tmp.crop((0,top,w,bottom))
