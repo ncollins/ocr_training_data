406915132df292c47f7dd319cf08ee7ef02e3d69
Nick Collins <nick@vividx.co.uk>
2013-02-15 19:04:13 -0500

    changed image sizes in sample.py

--- sample.py
+++ sample.py
@@ -3,17 +3,17 @@
 
 import Image, ImageDraw, ImageFont
 
-fontsize = 256
+fontsize = 64
 fonts = {
     "song": ImageFont.truetype(u"/Library/Fonts/华文宋体.ttf",fontsize),
 }
 
 text = u"您好，世界！"
 
-im = Image.new("L",(3000,300))
+im = Image.new("L",(400, 100))
 
 draw = ImageDraw.Draw(im)
 
 draw.text((20, 20), text, font=fonts["song"], fill=255)
 
-im_resized = im.resize((1000,100), Image.ANTIALIAS)
+im_resized = im.resize((200,50), Image.ANTIALIAS)
